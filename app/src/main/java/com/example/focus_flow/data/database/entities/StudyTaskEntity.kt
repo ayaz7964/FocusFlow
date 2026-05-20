23358a0112e2a1376a2ddd8cd3d81b325c84f8e4
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2026-05-21 00:17:28 +0500

    Changed The Fie

--- app/src/main/java/com/example/focus_flow/data/database/entities/StudyTaskEntity.kt
+++ app/src/main/java/com/example/focus_flow/data/database/entities/StudyTaskEntity.kt
@@ -1,4 +1,22 @@
 package com.example.focus_flow.data.database.entities
 
-class StudyTaskEntity {
-}
+//class StudyTaskEntity {
+//}
+
+
+//package com.focusflow.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "study_tasks")
+data class StudyTaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val subject: String,
+    val deadline: Long,
+    val priority: String,
+    val notes: String,
+    val status: String
+)
