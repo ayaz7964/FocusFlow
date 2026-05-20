4c2328696a21241f402ccad7ce51e63254e475b0
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2026-05-21 00:18:02 +0500

    Changed The Fie

--- app/src/main/java/com/example/focus_flow/data/database/entities/HabitEntity.kt
+++ app/src/main/java/com/example/focus_flow/data/database/entities/HabitEntity.kt
@@ -1,4 +1,22 @@
 package com.example.focus_flow.data.database.entities
 
-class HabitEntity {
-}
+//class HabitEntity {
+//}
+
+//package com.focusflow.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "habits")
+data class HabitEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val habitName: String,
+    val frequency: String,
+    val reminderTime: Long,
+    val iconRes: Int,
+    val streak: Int = 0,
+    val completedToday: Boolean = false,
+    val lastCompletedDate: Long = 0L
+)
